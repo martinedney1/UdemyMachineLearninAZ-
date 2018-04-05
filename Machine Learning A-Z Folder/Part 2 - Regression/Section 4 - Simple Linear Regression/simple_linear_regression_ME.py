09ec29659747bd61d42cb7eeaec80c0b15536b37
martinedney1 <32730883+martinedney1@users.noreply.github.com>
2018-04-05 14:45:43 +0100

    Finished simple linear

--- Machine Learning A-Z Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_ME.py
+++ Machine Learning A-Z Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression_ME.py
@@ -15,7 +15,7 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
@@ -30,3 +30,22 @@ y_train = sc_y.fit_transform(y_train)"""
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
+
+#Predicting the Test set
+y_pred = regressor.predict(X_test)
+
+# Visualisating the traininf set results
+plt.scatter(X_train,y_train,color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualisating the Yesy set results
+plt.scatter(X_test,y_test,color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
